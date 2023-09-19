d14cb2332b4c5c869bf072c9ef8f52c57c488510
InsightEdge01 <131486782+InsightEdge01@users.noreply.github.com>
2023-09-19 11:22:37 -0500

    Update app_with_charts.py

--- app_with_charts.py
+++ app_with_charts.py
@@ -3,7 +3,6 @@ from pandasai.llm.openai import OpenAI
 from dotenv import load_dotenv
 import os
 import pandas as pd
-#from pandasai import PandasAI
 from pandasai import SmartDataframe
 import matplotlib
 matplotlib.use('TkAgg')
@@ -17,9 +16,7 @@ os.environ["OPENAI_API_KEY"] = ""
 def chat_with_csv(df,prompt):
     llm = OpenAI(api_token=openai_api_key)
     pandas_ai = SmartDataframe(df, config={"llm": llm})
-    #pandas_ai = PandasAI(llm, save_charts=True)
     result = pandas_ai.chat(prompt)
-    #result = pandas_ai.run(df,prompt=prompt)
     return result
 
 st.set_page_config(layout='wide')
